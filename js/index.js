8a7149162e5bbfae4ea0523e3807d4be02f8a00f
tajkir-alam <tajkir.alam@gmail.com>
2023-03-21 19:32:39 +0600

    grid done

--- js/index.js
+++ js/index.js
@@ -9,17 +9,18 @@ const loadQuran = async() => {
 const displayQuran = (datas) => {
     const cardsContainer = document.getElementById('cards-container');
     datas.slice(0,15).map(data => {
-        // console.log(data.englishName)
-        const {name, englishName} = data;
+        console.log(data.ayahs[0].text.slice(40))
+        const {name, englishName, ayahs} = data;
         const div = document.createElement('div');
-        div.classList = "card w-96 bg-base-100 shadow-xl";
         div.innerHTML = `
-            <div class="card-body">
-                <h2 class="card-title">${name}</h2>
-                <h3 class="card-title">${englishName}</h3>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+            <div class="card h-64 w-auto bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="card-title text-2xl text-blue-700 justify-end">${name}</h2>
+                    <h3 class="card-title justify-">${englishName}</h3>
+                    <p class="text-right">${ayahs[0].text} ${ayahs[1].text}</p>
+                    <div class="card-actions justify-end">
+                      <button class="btn btn-primary">Buy Now</button>
+                    </div>
                 </div>
             </div>
         `
