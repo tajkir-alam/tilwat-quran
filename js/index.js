3424982e8893774b31b2337a5b785338fff907dd
tajkir-alam <tajkir.alam@gmail.com>
2023-03-21 22:15:57 +0600

    modal title done

--- js/index.js
+++ js/index.js
@@ -9,24 +9,39 @@ const loadQuran = async() => {
 const displayQuran = (datas) => {
     const cardsContainer = document.getElementById('cards-container');
     datas.slice(0,15).map(data => {
-        // console.log(data.ayahs[0].text.slice(40))
-        const {name, englishName, ayahs} = data;
+        // console.log(data.number)
+        const {name, englishName, ayahs, number} = data;
         const div = document.createElement('div');
         div.innerHTML = `
             <div class="card h-[17rem] w-auto bg-base-100 shadow-xl">
                 <div class="card-body">
                     <h2 class="card-title text-2xl text-blue-700 justify-end">${name}</h2>
-                    <h3 class="card-title justify-">${englishName}</h3>
+                    <h3 class="card-title">${englishName}</h3>
                     <p class="text-right">${ayahs[0].text.slice(0, 40)} ${ayahs[1].text.slice(0, 80)}</p>
                    
                     <div class="card-actions justify-end">
-                      <label for="my-modal" class="btn btn-primary tracking-wide">Tap to Read</label>
+                      <label for="my-modal" onclick="loadModal('${number}')" class="btn btn-primary tracking-wide">Tap to Read</label>
                     </div>
                 </div>
             </div>
         `
         cardsContainer.appendChild(div);
     })
+}
+
+const loadModal = async(number) => {
+    const URL = `https://api.alquran.cloud/v1/surah/${number}`;
+    const res = await fetch(URL);
+    const data = await res.json();
+    displayModal(data.data)
+}
+
+const displayModal = datas => {
+    const {name, englishName} = datas;
+    // console.log(name)
+    document.getElementById('modal-arabic-title').innerText = name;
+    document.getElementById('modal-english-title').innerText = englishName
+    
 }
 
 loadQuran();
