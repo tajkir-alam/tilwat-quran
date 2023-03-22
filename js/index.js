1d12aeb4bd63d2e09e9bdee0c638630d2acecbdf
tajkir-alam <tajkir.alam@gmail.com>
2023-03-22 17:58:00 +0600

    pagination click handler addded

--- js/index.js
+++ js/index.js
@@ -1,31 +1,34 @@
-const loadQuran = async() => {
+const loadQuran = async(dataLimitFrom, dataLimitTo) => {
     const URL = './js/completeQuran.json';
     const res = await fetch(URL);
     const data = await res.json();
-    displayQuran(data.data.surahs)
+    displayQuran(data.data.surahs, dataLimitFrom, dataLimitTo)
 }
 
-const displayQuran = (datas) => {
-    // console.log(datas)
+const displayQuran = (datas, dataLimitFrom, dataLimitTo) => {
     const cardsContainer = document.getElementById('cards-container');
-    datas.map(data => {
-        console.log(data.number)
-        const {name, englishName, ayahs, number} = data;
-        const div = document.createElement('div');
-        div.innerHTML = `
-            <div class="card h-[17rem] w-auto bg-base-100 shadow-xl">
-                <div class="card-body">
-                    <h2 class="card-title text-2xl text-blue-700 justify-end">${name}</h2>
-                    <h3 class="card-title">${englishName}</h3>
-                    <p class="text-right">${ayahs[0].text.slice(0, 40)} ${ayahs[1].text.slice(0, 80)}</p>
-                    <div class="card-actions justify-end">
-                      <label for="my-modal" onclick="loadModal('${number}')" class="btn btn-primary tracking-wide">Tap to Read</label>
+    cardsContainer.textContent = "";
+    // if(dataLimitFrom){
+
+        datas.slice(dataLimitFrom, dataLimitTo).map(data => {
+            const {name, englishName, ayahs, number} = data;
+            const div = document.createElement('div');
+            div.innerHTML = `
+                <div class="card h-[17rem] w-auto bg-base-100 shadow-xl">
+                    <div class="card-body">
+                        <h2 class="card-title text-2xl text-blue-700 justify-end">${name}</h2>
+                        <h3 class="card-title">${englishName}</h3>
+                        <p class="text-right">${ayahs[0].text.slice(0, 40)} ${ayahs[1].text.slice(0, 80)}</p>
+                        <div class="card-actions justify-end">
+                          <label for="my-modal" onclick="loadModal('${number}')" class="btn btn-primary tracking-wide">Tap to Read</label>
+                        </div>
                     </div>
                 </div>
-            </div>
-        `
-        cardsContainer.appendChild(div);
-    })
+            `
+            cardsContainer.appendChild(div);
+        })
+
+    // }
 }
 
 const loadModal = async(number) => {
@@ -47,4 +50,31 @@ const displayModal = datas => {
     document.getElementById('ayahs-are').innerHTML = eachSurah;
 }
 
-loadQuran(1);
+
+
+document.getElementById('page-1').addEventListener('click', function(){
+    loadQuran(0, 15)
+})
+document.getElementById('page-2').addEventListener('click', function(){
+    loadQuran(15, 30)
+})
+document.getElementById('page-3').addEventListener('click', function(){
+    loadQuran(30, 45)
+})
+document.getElementById('page-4').addEventListener('click', function(){
+    loadQuran(45, 60)
+})
+document.getElementById('page-5').addEventListener('click', function(){
+    loadQuran(60, 75)
+})
+document.getElementById('page-6').addEventListener('click', function(){
+    loadQuran(75, 90)
+})
+document.getElementById('page-7').addEventListener('click', function(){
+    loadQuran(90, 114)
+})
+
+
+window.addEventListener('load', function(){
+    loadQuran(0, 15);
+})
